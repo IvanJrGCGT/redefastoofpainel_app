79b7c631c275db9bc117a6717ae57282c9bbc68e
michellisx <100141052+michellisx@users.noreply.github.com>
2022-09-13 15:15:23 -0300

    michelle/homePage/header

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -20,7 +20,11 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
-          children: [],
+          children: [
+            Row(
+              children: [header()],
+            )
+          ],
         ),
       ),
     );
